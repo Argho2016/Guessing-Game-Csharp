4183f30ac495ebe4da7d89c9214313a61694b795
Argho Das <aiub2016@yahoo.com>
2021-11-23 22:59:31 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hiddenWord = "Spiderman";
+            string guess = "";
+
+            while(guess != hiddenWord)
+            {
+                Console.WriteLine("Enter Guess: ");
+                guess = Console.ReadLine();
+            }
+            Console.WriteLine("You Win!");
+
+            Console.ReadLine();
+        }
+    }
+}
