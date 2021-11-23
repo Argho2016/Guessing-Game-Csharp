a8183840117913684bf5f645f64123f158052bd8
Argho Das <aiub2016@yahoo.com>
2021-11-23 23:21:44 +0600

    Add files via upload

---
+++ GuessingGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Guessing_Game
+{
+    class GuessingGame
+    {
+        static void Main(string[] args)
+        {
+            string hiddenWord = "Spiderman";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outofGuess = false;
+
+            while(guess != hiddenWord && !outofGuess)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.WriteLine("Enter Guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }else
+                {
+                    outofGuess = true; //Flags, this is telling us a peice of info
+                }
+                
+            } if (outofGuess)
+            {
+                Console.WriteLine("You Lose!");
+
+            }
+            else
+            {
+                Console.WriteLine("You Win!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
